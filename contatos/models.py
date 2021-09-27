2d603392c0a92c6f095b4515eb37b9d5d2b69bc5
Samuel silva <samuel@debian.debian.com.br>
2021-09-27 12:23:46 -0300

    primeira configuraçao no model.py

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.deletion import DO_NOTHING
+from django.utils import timezone
 
-# Create your models here.
+
+class Categoria (models.Model):
+    nome = models.CharField(max_length=150)
+
+class Contato (models.Model):
+    nome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete= DO_NOTHING)
+
+    
