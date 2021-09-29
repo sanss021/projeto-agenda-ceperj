e1d95329f323c61851d977d278f092ab5e7f0cd7
Samuel silva <samuel@debian.debian.com.br>
2021-09-29 12:08:16 -0300

    configurandoo admin.py e model.py

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display_links = ('id','nome', 'sobrenome', 'sobrenome')
+    list_filter = ('nome', 'sobrenome')
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome','telefone')
+
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -6,6 +6,10 @@ from django.utils import timezone
 class Categoria (models.Model):
     nome = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.nome
+
+
 class Contato (models.Model):
     nome = models.CharField(max_length=150)
     sobrenome = models.CharField(max_length=255, blank=True)
@@ -14,5 +18,8 @@ class Contato (models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete= DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
 
     
