34250884ac6ac9dac0fe80d195e7bd568d75c201
Samuel silva <samuel@debian.debian.com.br>
2021-10-01 11:40:48 -0300

    configuando views

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+import contatos
+from .models import Contato
+
 def index(request):
-    return render (request, 'contatos/index.html')
+    contatos = Contato.objects.all()
+    return render (request, 'contatos/index.html', {
+        'contatos': contatos
+    })
