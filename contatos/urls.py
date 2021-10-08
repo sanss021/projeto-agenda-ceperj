c0605a96dc1b3b4b841b97d6d6d88ba43bf782e7
sanss021 <smlmonteiroum@gmail.comm>
2021-10-08 12:31:44 -0300

    funcionamento do campo de busca

--- contatos/urls.py
+++ contatos/urls.py
@@ -1,9 +1,11 @@
+from functools import partial
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:contato_id>', views.ver_contato,  name='ver_contato'),
+    path('busca/', views.busca, name='busca')
     
     
 
--- contatos/views.py
+++ contatos/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
 from django.http import Http404
-
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 def index(request):
 	# var = model.objects.order_by('-data_criacao')
@@ -25,6 +26,23 @@ def ver_contato(request, contato_id):
 
 	return render(request, 'contatos/ver_contato.html',{
 		'contato': contato
+	
+	})
+
+def busca(request):
+	termo = request.GET.get('termo')
+
+	campos = Concat('nome', Value(''), 'sobrenome')
+	
+	contatos = Contato.objects.annotate(
+		nome_completo = campos).filter( Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo))
+
+	paginator = Paginator(contatos, 5)
+	page = request.GET.get('p')
+	contatos= paginator.get_page(page)
+
+	return render(request, 'contatos/index.html',{
+		'contatos': contatos
 	
 	})
 
