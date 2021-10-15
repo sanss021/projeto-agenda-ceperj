eadb13594ccf3743160f56955be316d44d3c14df
sanss021 <smlmonteiroum@gmail.comm>
2021-10-15 09:47:39 -0300

    trabalhando com imagens

--- contatos/models.py
+++ contatos/models.py
@@ -19,6 +19,7 @@ class Contato (models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete= DO_NOTHING)
     mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m/')
 
     def __str__(self):
         return self.nome
