5ccc2718551a9e748cbe1035da5821481d8a2baa
sanss021 <smlmonteiroum@gmail.comm>
2021-10-22 12:46:54 -0300

    formularios a partir de models e footer

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import FormContato
 
 
 
@@ -84,6 +85,26 @@ def cadastro(request):
 
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    if request.method != 'POST':
+        form = FormContato()
+        return render(request, 'accounts/dashboard.html', {'form':form})
+
+    form = FormContato(request.POST, request.FILES)
+
+    if not form.is_valid():
+        messages.error(request, 'Erro ao enviar o formulário.')
+        form = FormContato()
+        return render(request, 'accounts/dashboard.html', {'form':form})
+    
+    descricao = request.POST.get('descricao')
+
+    if len(descricao) < 5:
+        messages.error(request, 'Descrição precisar ser maior que 5 caracteres.')
+        form = FormContato()
+        return render(request, 'accounts/dashboard.html', {'form':form})
+
+    form.save()
+    messages.success(request, f'Contato {request.POST.get("nome")} Salvo com sucesso!')
+    return redirect('dashboard')
 
 
